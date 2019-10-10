56f95f0b1cb0f7a6e504f951dcd0116d2a8173fd
kanyeTheGreat <ALEXIS.CHAVEZ193@tectijuana.edu.mx>
2019-10-10 09:26:09 -0700

    Este es mi primer push desde mi repo local hacia mi repo en la nube

---
+++ elEmpalador/elEmpalador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elEmpalador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, soy tu dueño, este es mi primer repositorio");
+            Console.ReadKey();
+        }
+    }
+}
